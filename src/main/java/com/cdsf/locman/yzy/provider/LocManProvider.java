d4b968352a63c1b6cc9e7e3063888c3f9c5dce90
吴文亮 <wuwenliang@ACA8016D.ipt.aol.com>
2017-05-16 10:56:27 +0800

    remove arounter

--- src/main/java/com/cdsf/locman/yzy/provider/LocManProvider.java
+++ src/main/java/com/cdsf/locman/yzy/provider/LocManProvider.java
@@ -2,8 +2,6 @@ package com.cdsf.locman.yzy.provider;
 
 import android.content.Context;
 
-import com.alibaba.android.arouter.facade.template.IProvider;
-
 /**
  * Created by wuwenliang on 2017/5/12.
  */
